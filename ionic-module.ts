f25560663184f31b9810f1959fcfaae8c18a5d1d
Randy Sagum <randy.sagum@yahoo.com>
2016-11-21 15:31:44 +0800

    Working on user regis

--- ionic-module.ts
+++ ionic-module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
 const cloudSettings: CloudSettings = {
   'core': {
-    'app_id': '2e401812'
+    //'app_id': '2e401812'
+    'app_id': '46ab1702'
   }
 };
 @NgModule({
